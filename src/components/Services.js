742ae8a3daf62c7a9b4c6e097eb2b98ca6682d43
edwarddou2020 <zuochao.dou@nvxclouds.com>
2024-02-17 21:53:41 -0500

    minor update

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,17 +7,15 @@ function Services() {
             <div className="col-md-12">
               <div className="row">
                 <div className="col-md-4 mb-2">
-                  <Card title="Web Development Solutions" img="card1.png"
-                        text="\n
-                        Web services design, development and maintenance. "/>
+                  <Card title="                                     Web Development Solutions" img="card1.png"
+                        text="Web services design, development and maintenance. "/>
                 </div>
                 <div className="col-md-4 mb-2">
                   <Card title="Data Privacy Consulting" img="card2.png"
                         text="Personal data security, large data secure transfer."/>
                 </div>
                 <div className="col-md-4 mb-2">
-                  <Card title="\n
-                  Privacy Computing Solutions" img="card3.png"
+                  <Card title="                                     Privacy Computing Solutions" img="card3.png"
                         text="Key management service, federated learning, secure multi-party computing."/>
                 </div>
               </div>
