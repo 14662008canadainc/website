185c7b181fc0af7be3c08b8c8a9dac55af5a4050
edwarddou2020 <zuochao.dou@nvxclouds.com>
2024-05-29 21:09:17 -0400

    about

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ function About() {
             <h2 className="main-title about-h2">ABOUT</h2>
             <p className="main-p">
               At E2A2 INNOVATIONS, we design, build, scale and deliver customized software solutions.{"\n"}
-              Working with Lavita @ Singapore, a blockchain-AI based next generation healthcare technology company aiming to revolutionize the way 8 billion individuals around the world diagnose, analyze, and treat health and genomic issues, leading to earlier intervention and improved outcome.{"\n"}
+              Working with Lavita @ Singapore, a blockchain-AI based next generation healthcare technology company aiming to revolutionize the way 8 billion individuals around the world diagnose, analyze, and treat health & genomic issues, leading to earlier intervention and improved outcome.{"\n"}
               Working with NovoVivo @ California, building a platform to enable the use of massive biomedical datasets for research while preserving individual privacy and ownership of data.
             </p>
           </div>
